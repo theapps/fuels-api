d28dfc6beebdb89f7792a0a80e0ddcbd3c333024
daiot <stefanescu.dragos@gmail.com>
2018-10-04 21:43:47 +0300

    ui updated

---
+++ Migrations/20181004174344_FuelsToDbCtx.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class FuelsToDbCtx : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Fuel_Vehicles_VehicleId",
+                table: "Fuel");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Fuel",
+                table: "Fuel");
+
+            migrationBuilder.RenameTable(
+                name: "Fuel",
+                newName: "Fuels");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Fuel_VehicleId",
+                table: "Fuels",
+                newName: "IX_Fuels_VehicleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Fuels",
+                table: "Fuels",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Fuels_Vehicles_VehicleId",
+                table: "Fuels",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Fuels_Vehicles_VehicleId",
+                table: "Fuels");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Fuels",
+                table: "Fuels");
+
+            migrationBuilder.RenameTable(
+                name: "Fuels",
+                newName: "Fuel");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Fuels_VehicleId",
+                table: "Fuel",
+                newName: "IX_Fuel_VehicleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Fuel",
+                table: "Fuel",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Fuel_Vehicles_VehicleId",
+                table: "Fuel",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ wwwroot/js/about.326ff8b5.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var u=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("evidenta consum si bonuri de carburant")])])}],s=n("2877"),i={},o=Object(s["a"])(i,u,a,!1,null,null,null);o.options.__file="About.vue";e["default"]=o.exports}}]);
+//# sourceMappingURL=about.326ff8b5.js.map
